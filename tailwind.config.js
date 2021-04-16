67653fc17e9cc90736fdd3ff55b6d7cf5120b174
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-04-15 21:49:30 -0300

    chore: Add purge css config to include dynamic classes, and add base url on vite build config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: {
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    options: {
+      safelist: [/^grid-rows-/, /^grid-cols-/, /^gap-/],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       animation: {
         pulse: "pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
-    }
+    },
   },
   variants: {
     extend: {
-      backgroundColor: ['active'], 
+      backgroundColor: ["active"],
     },
   },
   plugins: [],
-}
+};
