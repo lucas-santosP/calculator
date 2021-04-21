4f35132c7c3545eae9d8d4e8b7dc653e48399fcf
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-04-21 12:58:19 -0300

    refactor: Move calculator logic to useCalculate compostition

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -11,17 +11,17 @@
     <Button variant="yellow" @click="eraseLast">Del</Button>
     <Button variant="green" @click="addOperator('/')">/</Button>
 
-    <Button variant="blue" v-for="number in padNumbers[0]" :key="number" @click="addDigit(number)">
+    <Button variant="blue" v-for="number in [7, 8, 9]" :key="number" @click="addDigit(number)">
       {{ number }}
     </Button>
     <Button variant="green" @click="addOperator('*')">*</Button>
 
-    <Button variant="blue" v-for="number in padNumbers[1]" :key="number" @click="addDigit(number)">
+    <Button variant="blue" v-for="number in [4, 5, 6]" :key="number" @click="addDigit(number)">
       {{ number }}
     </Button>
     <Button variant="green" @click="addOperator('-')">-</Button>
 
-    <Button variant="blue" v-for="number in padNumbers[2]" :key="number" @click="addDigit(number)">
+    <Button variant="blue" v-for="number in [1, 2, 3]" :key="number" @click="addDigit(number)">
       {{ number }}
     </Button>
     <Button variant="green" @click="addOperator('+')">+</Button>
@@ -33,7 +33,8 @@
 </template>
 
 <script lang="ts">
-import { ref, defineComponent, onMounted, onBeforeUnmount } from "vue";
+import { defineComponent, onBeforeUnmount, onMounted } from "vue";
+import { useCalculate } from "../compositions/useCalculate";
 import { useKeyboard } from "../compositions/useKeyboard";
 import Button from "./Button.vue";
 import Screen from "./Screen.vue";
@@ -42,8 +43,8 @@ import {
   DIGITS_KEYS,
   OPERATORS_KEYS,
   RESULT_KEYS,
-  ERASE_KEYS,
   CLEAR_KEYS,
+  ERASE_KEYS,
 } from "../shared/constants";
 
 export default defineComponent({
@@ -51,26 +52,18 @@ export default defineComponent({
   components: { Button, Screen, Grid },
 
   setup: () => {
-    const padNumbers = [
-      [7, 8, 9],
-      [4, 5, 6],
-      [1, 2, 3],
-    ];
-    const operators = ["/", "*", "+", "-"];
-    let memory = ref("");
-    let error = ref(false);
-    let clearOnNextDigit = ref(false);
+    const calculate = useCalculate();
     const keyboard = useKeyboard();
 
     onMounted(() => {
       keyboard.addListener((e) => {
         const key = e.key === "," ? "." : e.key;
 
-        if (DIGITS_KEYS.includes(key)) addDigit(key);
-        if (OPERATORS_KEYS.includes(key)) addOperator(key);
-        if (RESULT_KEYS.includes(key)) calculateResult();
-        if (ERASE_KEYS.includes(key)) eraseLast();
-        if (CLEAR_KEYS.includes(key)) clear();
+        if (DIGITS_KEYS.includes(key)) calculate.addDigit(key);
+        if (OPERATORS_KEYS.includes(key)) calculate.addOperator(key);
+        if (RESULT_KEYS.includes(key)) calculate.calculateResult();
+        if (ERASE_KEYS.includes(key)) calculate.eraseLast();
+        if (CLEAR_KEYS.includes(key)) calculate.clear();
       });
     });
 
@@ -78,80 +71,7 @@ export default defineComponent({
       keyboard.removeAllListeners();
     });
 
-    function isOperator(string: string) {
-      return operators.find((operator) => operator === string);
-    }
-
-    function lastCharIsOperator(string: string) {
-      const stringNormalized = string.replace(/\s/g, "");
-      return isOperator(stringNormalized[stringNormalized.length - 1]);
-    }
-
-    function addDigit(digit: number | string) {
-      const lastDigit = memory.value[memory.value.length - 1];
-
-      if (lastDigit === "." && digit === ".") return;
-      if (lastDigit === "0" && memory.value.length === 1) clear();
-      if (clearOnNextDigit.value) clear();
-      if ((!memory.value || lastCharIsOperator(memory.value)) && digit === ".") memory.value += "0";
-
-      clearOnNextDigit.value = false;
-      memory.value += `${digit}`;
-    }
-
-    function addOperator(operator: string) {
-      if (!memory.value) return;
-      if (lastCharIsOperator(memory.value)) eraseLast();
-
-      clearOnNextDigit.value = false;
-      memory.value += ` ${operator} `;
-    }
-
-    function calculateResult() {
-      if (!memory.value) return;
-
-      let mathExpression = memory.value.replace(/\s/g, ""); //remove spaces
-      if (lastCharIsOperator(mathExpression)) {
-        mathExpression = mathExpression.slice(0, mathExpression.length - 1);
-      }
-
-      try {
-        mathExpression = mathExpression.replace(/\b0*((\d+\.\d+|\d+))\b/g, "$1"); // remove octal numeric
-        memory.value = `${eval(mathExpression) || ""}`;
-      } catch (_) {
-        error.value = true;
-        memory.value = "";
-      } finally {
-        clearOnNextDigit.value = true;
-      }
-    }
-
-    function eraseLast() {
-      if (!memory.value.length) return;
-
-      if (lastCharIsOperator(memory.value)) {
-        memory.value = memory.value.slice(0, memory.value.length - 3); // remove spaces too
-      } else {
-        memory.value = memory.value.slice(0, memory.value.length - 1);
-      }
-      clearOnNextDigit.value = false;
-    }
-
-    function clear() {
-      memory.value = "";
-      error.value = false;
-    }
-
-    return {
-      memory,
-      error,
-      padNumbers,
-      addDigit,
-      addOperator,
-      calculateResult,
-      eraseLast,
-      clear,
-    };
+    return { ...calculate };
   },
 });
 </script>
---
+++ src/compositions/useCalculate.ts
@@ -0,0 +1,75 @@
+import { ref } from "vue";
+
+const OPERATORS = ["/", "*", "+", "-"];
+
+export function useCalculate() {
+  let memory = ref("");
+  let error = ref(false);
+  let clearOnNextDigit = ref(false);
+
+  function isOperator(string: string) {
+    return OPERATORS.includes(string);
+  }
+
+  function lastCharIsOperator(string: string) {
+    const stringNormalized = string.replace(/\s/g, "");
+    return isOperator(stringNormalized[stringNormalized.length - 1]);
+  }
+
+  function addDigit(digit: number | string) {
+    const lastDigit = memory.value[memory.value.length - 1];
+
+    if (lastDigit === "." && digit === ".") return;
+    if (lastDigit === "0" && memory.value.length === 1) clear();
+    if (clearOnNextDigit.value) clear();
+    if ((!memory.value || lastCharIsOperator(memory.value)) && digit === ".") memory.value += "0";
+
+    clearOnNextDigit.value = false;
+    memory.value += `${digit}`;
+  }
+
+  function addOperator(operator: string) {
+    if (!memory.value) return;
+    if (lastCharIsOperator(memory.value)) eraseLast();
+
+    clearOnNextDigit.value = false;
+    memory.value += ` ${operator} `;
+  }
+
+  function calculateResult() {
+    if (!memory.value) return;
+
+    let mathExpression = memory.value.replace(/\s/g, ""); //remove spaces
+    if (lastCharIsOperator(mathExpression)) {
+      mathExpression = mathExpression.slice(0, mathExpression.length - 1);
+    }
+
+    try {
+      mathExpression = mathExpression.replace(/\b0*((\d+\.\d+|\d+))\b/g, "$1"); // remove octal numeric
+      memory.value = `${eval(mathExpression) || ""}`;
+    } catch (_) {
+      error.value = true;
+      memory.value = "";
+    } finally {
+      clearOnNextDigit.value = true;
+    }
+  }
+
+  function eraseLast() {
+    if (!memory.value.length) return;
+
+    if (lastCharIsOperator(memory.value)) {
+      memory.value = memory.value.slice(0, memory.value.length - 3); // remove spaces too
+    } else {
+      memory.value = memory.value.slice(0, memory.value.length - 1);
+    }
+    clearOnNextDigit.value = false;
+  }
+
+  function clear() {
+    memory.value = "";
+    error.value = false;
+  }
+
+  return { memory, error, addDigit, addOperator, calculateResult, eraseLast, clear };
+}
